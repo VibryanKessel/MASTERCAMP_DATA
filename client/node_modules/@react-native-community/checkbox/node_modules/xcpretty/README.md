3643d1fb381e61881cf0b3411fc47be922cc6a2d
ZERO <zero20022003@gmail.com>
2022-06-23 02:57:04 +0200

    login version 1

---
+++ client/node_modules/@react-native-community/checkbox/node_modules/xcpretty/README.md
@@ -0,0 +1,10 @@
+# XcodeGen
+
+![logo](http://i.imgur.com/i2fElxx.png)
+
+This is a binary consumable via esy/npm.
+
+Sources can be found at https://github.com/supermarin/xcpretty
+
+**`xcpretty` is a fast and flexible formatter for `xcodebuild`**.<br/>
+It does one thing, and it should do it well.
---
+++ client/src/components/Login.js
@@ -0,0 +1,64 @@
+
+import React, { useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native"
+import tw from "twrnc"
+
+export default () => {
+    const [isSelected, setSelection] = useState(false);
+    
+    return( 
+    <View style = {tw`flex flex-row m-0 h-full w-full`}>
+        <View style = {tw`h-full w-1/2 px-10 py-40`}>
+            <View style = {tw`flex flex-col h-5/6 font-medium text-center rounded-lg`}>
+                <Text style = {tw`text-3xl font-black`}>
+                    WELCOME   BACK
+                </Text>
+                <Text style = {tw`text-gray-300 border-b border-gray-300 w-full`}>
+                    Welcome back! Please enter your details. 
+                </Text>
+                <View style = {tw`px-0`}>    
+                    <View style = {tw`py-5`}>
+                        <Text style = {tw`text-lg text-left`}>
+                            Email
+                        </Text>
+                        <TextInput placeholder="Enter your Email" style = {tw`border-2 text-xl h-10 rounded `}/>
+                    </View>
+                    <View style = {tw`py-5`}>
+                        <Text style = {tw`text-lg text-left`}>
+                            Password
+                        </Text>
+                        <TextInput placeholder="Enter your Password" style = {tw`border-2 text-xl h-10 rounded `}/>
+                    </View>
+                    <TouchableOpacity style = {tw`py-5 `}>
+                        <Text style = {tw`font-bold text-right text-indigo-500 `}>
+                            Forgot password
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style = {tw`shadow-lg shadow-gray-500/50 border-2 border-orange-500 bg-orange-500 font-black h-10 rounded`}>
+                        <Text style = { tw` text-gray-100 text-xl text-center font-bold`}>
+                            SIGN IN
+                        </Text>
+                    </TouchableOpacity>
+                    <View style = {tw`flex flex-row py-5 self-center`}>
+                        <Text>
+                            Don't yet have an account ?
+                        </Text>
+                        <TouchableOpacity style = { tw`px-2` }>
+                            <Text style = {tw`font-bold underline text-indigo-500 `}>
+                                Sign up
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            <Text style = { tw`absolute bottom-2 left-2 text-gray-500`}>
+                ©_____________2022
+            </Text>    
+        </View>
+        <View style = {tw`h-full w-1/2 bg-gray-200 py-50`}>
+            <Image source={ require('../../assets/logo.jpeg') }
+            style = { {...tw`rounded-full self-center`,width : 300,height : 300} }
+            />        
+        </View>
+    </View>
+)}
