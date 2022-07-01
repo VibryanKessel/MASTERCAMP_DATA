05d77348d84ee6d7135db8faa528218fdc64f081
Aure-M <marcaurelalioza@gmail.com>
2022-07-01 18:14:06 +0200

    Commit Reommandation

--- server/routes/recommandation.js
+++ server/routes/recommandation.js
@@ -1,39 +1,54 @@
 const express = require('express');
 const axios = require("axios");
+const asyncHandler = require('express-async-handler')
 const { getIngredients, categorizeIngredients, combine } = require('../utils/dataProcessing');
 const baseUrl = "https://api.spoonacular.com/recipes"
 
 const loginRouter = (con) => {
     const router = express.Router();
+
     /*----------------------------------------------------- */
-    router.get("/", (req, res) => {
-        let ingredients = null;
 
-        getIngredients(con).then(values => {
-            ingredients = categorizeIngredients(values);
-            const combinaisons_ingredients = combine(ingredients);
-            console.log(combinaisons_ingredients);
+    router.get('/', asyncHandler(async (req, res, next) => {
+        
+        const recipes = await getRecipesFromApi(con)
+        console.log(recipes)
+        res.status(200).json(recipes);
+    })); 
+
+/*---------------------------------------------------------- */
+return router;
+}
 
 
-            const response = axios.get(baseUrl + "/findByIngredients", {
+
+
+function getRecipesFromApi(con)
+{
+    return new Promise(async (resolve,reject)=>{
+
+        let ingredients = await getIngredients(con);
+        ingredients = categorizeIngredients(ingredients);
+        
+        const combinaisons_ingredients = combine(ingredients); //combine sert à créer des combinaisons différentes d'ingrédients
+        console.log(combinaisons_ingredients);
+        
+        /* Pour chaque combinaison on va demander des recettes qu'elles peuvent constituer */
+        let recipes = []
+        for (let i = 0; i < combinaisons_ingredients.length; i++) {
+            let response = await axios.get(baseUrl + "/findByIngredients", {
                 params: {
-                    "ingredients": combinaisons_ingredients[0],
+                    "ingredients": combinaisons_ingredients[i],
                     "apiKey": "29cb37909ef84b8d867a0350baee367d",
                     "number": 10
                 }
             });
 
-            response.then(result => {
-                res.status(200).json(result.data);
-            });
+            recipes = recipes.concat(response.data);
+        }
 
-        });
-        return;
-    })
-
-
-    /*---------------------------------------------------------- */
-    return router;
-}
+        resolve(recipes);
+    });
+};
 
 module.exports = loginRouter;
