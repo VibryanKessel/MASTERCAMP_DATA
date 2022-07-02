ef2558c9f4a89f6cbc4991003535c01af27c5aac
Aure-M <marcaurelalioza@gmail.com>
2022-07-03 00:45:14 +0200

    commit Aurel

--- server/routes/recommandation.js
+++ server/routes/recommandation.js
@@ -2,6 +2,8 @@ const express = require('express');
 const axios = require("axios");
 const asyncHandler = require('express-async-handler')
 const { getIngredients, categorizeIngredients, combine } = require('../utils/dataProcessing');
+const { getRecipesFromApiBaseOnIngredients } = require('../utils/api_functions');
+
 const baseUrl = "https://api.spoonacular.com/recipes"
 
 const loginRouter = (con) => {
@@ -11,8 +13,13 @@ const loginRouter = (con) => {
 
     router.get('/', asyncHandler(async (req, res, next) => {
 
-        const recipes = await getRecipesFromApi(con)
+        const recipes = await getRecipesFromApiBaseOnIngredients(con)
         console.log(recipes)
+        res.status(200).json(recipes);
+    }));
+
+    router.get('/forDiet/:idRegime', asyncHandler(async (req, res, next) => {
+
         res.status(200).json(recipes);
     }));
 
@@ -23,40 +30,5 @@ const loginRouter = (con) => {
 
 
 
-function getRecipesFromApi(con) {
-    return new Promise(async (resolve, reject) => {
-
-        let ingredients = await getIngredients(con);
-        ingredients = categorizeIngredients(ingredients);
-
-        const combinaisons_ingredients = combine(ingredients); //combine sert à créer des combinaisons différentes d'ingrédients
-        console.log(combinaisons_ingredients);
-
-        /* Pour chaque combinaison on va demander des recettes qu'elles peuvent constituer */
-        let recipes = []
-        for (let i = 0; i < combinaisons_ingredients.length; i++) {
-            let response = await axios.get(baseUrl + "/findByIngredients", {
-                params: {
-                    "ingredients": combinaisons_ingredients[i],
-                    "apiKey": "29cb37909ef84b8d867a0350baee367d",
-                    "number": 10
-                }
-            });
-
-            recipes = recipes.concat(response.data);
-        }
-
-        for (let i = 0; i < recipes.length; i++) {
-
-            recipes[i] = {
-                "id": recipes[i].id,
-                "title": recipes[i].title,
-                "image": recipes[i].image,
-            }
-        }
-
-        resolve(recipes);
-    });
-};
 
 module.exports = loginRouter;
